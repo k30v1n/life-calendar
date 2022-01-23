7f11b66d8e7702bf503c774c0d00b873e22ecba1
Kelvin Ferreira <10376375+k30v1n@users.noreply.github.com>
2022-01-23 02:50:38 -0500

    fetching from ui

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public yearsOfLife: YearOfLife[] = [];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<LifeCalendar>(baseUrl + 'lifecalendar').subscribe(result => {
+      this.yearsOfLife = result.yearsOfLife;
+    }, error => console.error(error));
+  }
+}
+
+interface LifeCalendar {
+  yearsOfLife: YearOfLife[];
+}
+
+interface YearOfLife {
+  age:Number;
+  year:Number;
+  weeks:Week[];
+}
+
+interface Week {
+  lived:Boolean;
+  date:Date;
 }
